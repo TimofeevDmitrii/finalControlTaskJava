0e398e0ab907257e42168557bae26ed5d3e33a4b
Dmitry <dimatimoha2014@gmail.com>
2024-02-22 11:39:56 +0300

    1)class StoreElement renamed to ToyStoreElement;abstract class StoreElement was created and ToyStoreElement now extends this class; interface CreateNewToy renamed to CreateToyElement; 2) The lottery of toys was tested with another algorithm with priority queue

--- src/main/java/Data/ToyElementDataParser.java
+++ src/main/java/Data/ToyElementDataParser.java
@@ -1,25 +1,27 @@
+package Data;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ToyCreator implements CreateNewToy{
+public class ToyElementDataParser implements CreateToyElement {
     private int nextToyId=1;
 
     private boolean checkToyData(String toyData){
-        Pattern pattern = Pattern.compile("^[а-яА-Я]{1,20}\\s\\d{1,2}$");
+        Pattern pattern = Pattern.compile("^[а-яА-Я]{1,20}\\s\\d*\\.?\\d+\\s\\d+$");
         Matcher matcher = pattern.matcher(toyData);
         return matcher.find();
     }
 
 
     @Override
-    public Toy createToy(String toyData) {
+    public ToyStoreElement createToyElement(String toyData) {
         if (!checkToyData(toyData)){
             throw new RuntimeException("Введены неверные данные для добавления игрушки");
         }
         else {
             String[] splitToyData = toyData.split(" ");
-            System.out.println();
-            return new Toy(nextToyId++, splitToyData[0], Double.parseDouble(splitToyData[1]));
+            Toy newToy = new Toy(nextToyId++, splitToyData[0], Double.parseDouble(splitToyData[1]));
+            return new ToyStoreElement(newToy, Integer.parseInt(splitToyData[2]));
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import Data.Toy;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -20,51 +22,68 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Toy doll = new Toy(1, "Кукла", 2);
-        Toy robot = new Toy(2, "Робот", 6);
-        Toy constructor = new Toy(3, "Конструктор", 2);
-        Toy helicopter = new Toy(4, "Вертолет", 3);
-
-        int totalWeight = doll.toyWeight + robot.toyWeight + constructor.toyWeight + helicopter.toyWeight;
-
-        HashMap<Toy, ArrayList<Integer>> weightNumbers = new HashMap<>();
-        weightNumbers.put(doll, new ArrayList<>());
-        weightNumbers.put(robot, new ArrayList<>());
-        weightNumbers.put(constructor, new ArrayList<>());
-        weightNumbers.put(helicopter, new ArrayList<>());
-
-        int count = 1;
-        for (Toy toy : weightNumbers.keySet()) {
-            for (int i = 0; i < toy.toyWeight; i++) {
-                weightNumbers.get(toy).add(count++);
-            }
-        }
-        System.out.println(weightNumbers);
+        ModelToyShop model = new ModelToyShop();
 
 
-        HashMap<Toy, Integer> results = new HashMap<>();
-        results.put(doll, 0);
-        results.put(robot, 0);
-        results.put(constructor, 0);
-        results.put(helicopter, 0);
+//        model.putToyToStore("Вертолет 3 10");
+//        model.putToyToStore("Танк 4.5 10");
+//        model.putToyToStore("Самолет 2 10");
+//        model.putToyToStore("Робот 6 10");
+
+        model.putToyToStore("Конструктор 3 10");
+        model.putToyToStore("Робот 4 10");
+        model.putToyToStore("Робот 2 10");
+        model.putToyToStore("Кукла 6 10");
 
 
-        int rndNumber;
+        model.getPrize(553);
 
-        int k=0;
-        while(k!=1000){
-            rndNumber = 1 + (int) (Math.random() * totalWeight); // генерируем числа от 1 до 10:
-            for (Toy toy: weightNumbers.keySet()){
-                if (weightNumbers.get(toy).contains(rndNumber)) {
-                    results.put(toy,results.get(toy)+1);
-                    break;
-                }
-            }
-            k++;
-        }
 
-        System.out.println(results);
-
+//        Toy doll = new Toy(1, "Кукла", 2);
+//        Toy robot = new Toy(2, "Робот", 6);
+//        Toy constructor = new Toy(3, "Конструктор", 2);
+//        Toy helicopter = new Toy(4, "Вертолет", 3);
+//
+//        int totalWeight = doll.toyWeight + robot.toyWeight + constructor.toyWeight + helicopter.toyWeight;
+//
+//        HashMap<Toy, ArrayList<Integer>> weightNumbers = new HashMap<>();
+//        weightNumbers.put(doll, new ArrayList<>());
+//        weightNumbers.put(robot, new ArrayList<>());
+//        weightNumbers.put(constructor, new ArrayList<>());
+//        weightNumbers.put(helicopter, new ArrayList<>());
+//
+//        int count = 1;
+//        for (Toy toy : weightNumbers.keySet()) {
+//            for (int i = 0; i < toy.toyWeight; i++) {
+//                weightNumbers.get(toy).add(count++);
+//            }
+//        }
+//        System.out.println(weightNumbers);
+//
+//
+//        HashMap<Toy, Integer> results = new HashMap<>();
+//        results.put(doll, 0);
+//        results.put(robot, 0);
+//        results.put(constructor, 0);
+//        results.put(helicopter, 0);
+//
+//
+//        int rndNumber;
+//
+//        int k=0;
+//        while(k!=1000){
+//            rndNumber = 1 + (int) (Math.random() * totalWeight); // генерируем числа от 1 до 10:
+//            for (Toy toy: weightNumbers.keySet()){
+//                if (weightNumbers.get(toy).contains(rndNumber)) {
+//                    results.put(toy,results.get(toy)+1);
+//                    break;
+//                }
+//            }
+//            k++;
+//        }
+//
+//        System.out.println(results);
+//
 
     }
 }
